7ecc7d1a5184ee1726bf70b334f62f324a4e4b21
p-koskey <patiencekoskey@gmail.com>
2020-09-13 16:32:08 +0300

    update .gitignore and created an executable file

--- tests/source_test.py
+++ tests/source_test.py
@@ -1,22 +1,16 @@
 import unittest
-from models import source
+from app.models import Source
 
-Source = source.Source
-
-class  SourceTest(unittest.TestCase):
+class SourceTest(unittest.TestCase):
     '''
-    Testing behaviour of source class
+    Test Class to test the behaviour of the Source class
     '''
 
     def setUp(self):
         '''
-        Set up method that will run before every test
+        Set up method that will run before every Test
         '''
-        self.news_source = Source('abc-news','ABC News', 'https://abcnews.go.com')
+        self.new_source = Source('abc-news','ABC News')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.news_source,Source))
-
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertTrue(isinstance(self.new_source,Source))
